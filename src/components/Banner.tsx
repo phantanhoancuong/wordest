0bebba8f589ec3b1eea5474e4b4ceba335eb5580
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2025-12-31 19:42:09 +0700

    refactor(Banner): use Link for logo navigation
    Update the Banner component so that clicking the logo uses Next.js <Link> instead of an inline onClick with window.location.href="/".
    
    This was done to enable preservation of React states when the user navigates back and forth between pages as the previous implementation did full page reloads when navigating from the logo.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,8 @@
-import styles from "../styles/Banner.module.css";
 import Image from "next/image";
 import Link from "next/link";
+
+import styles from "@/styles/Banner.module.css";
+
 import settingsIcon from "@/assets/icons/settings_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg";
 
 /**
@@ -10,13 +12,9 @@ const Banner = () => (
   <div className={styles["banner"]}>
     <div className={styles["banner__left"]}>
       <div className={styles["banner__logo"]}>
-        <Image
-          src="/images/logo.svg"
-          alt="Logo"
-          fill
-          priority
-          onClick={() => (window.location.href = "/")}
-        />
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="Logo" fill priority />
+        </Link>
       </div>
     </div>
     <div className={styles["banner__right"]}>
