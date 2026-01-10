bdf975d0f99860bf140971dd1fafc77c8567c2fa
Phan Tân Hoàn Cương <phantanhoancuong@gmail.com>
2026-01-10 22:22:03 +0700

    chore(layout): move viewport from metadata
    - Move viewport into its own export from metadata to comply with Next.js 13+ requirements.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,12 @@ export const metadata = {
   icons: {
     icon: "images/logo-small.svg",
   },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({ children }) {
